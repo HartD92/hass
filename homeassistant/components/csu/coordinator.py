63a07d6f1ffca6fc2ec2e4aa39d200fce094f044
David Hart <hartd92@gmail.com>
2024-06-26 16:36:45 -0600

    initial commit of CSU

---
+++ homeassistant/components/csu/coordinator.py
@@ -0,0 +1,191 @@
+from datetime import datetime, timedelta
+import logging
+import socket
+from types import MappingProxyType
+from typing import Any, cast
+
+from csu import (
+    CSU,
+    AggregateType,
+    Meter,
+    MeterType,
+    ReadResolution,
+    UsageRead,
+)
+
+from homeassistant.components.recorder import get_instance
+from homeassistant.components.recorder.models import StatisticData, StatisticMetaData
+from homeassistant.components.recorder.statistics import (
+    async_add_external_statistics,
+    get_last_statistics,
+    statistics_during_period,
+)
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, UnitOfEnergy, UnitOfVolume
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers import aiohttp_client
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from homeassistant.util import dt as dt_util
+
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class CsuCoordinator(DataUpdateCoordinator[dict[str, UsageRead]]):
+    """Handle fetching CSU data, updating sensors and inserting statistics."""
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        entry_data: MappingProxyType[str, Any],
+    ) -> None:
+        """Initialize the data handler."""
+        super().__init__(
+            hass,
+            _LOGGER,
+            name="Csu",
+            update_interval=timedelta(hours=12),
+        )
+        self.api = CSU(
+            aiohttp_client.async_get_clientsession(hass),
+            entry_data[CONF_USERNAME],
+            entry_data[CONF_PASSWORD],
+        )
+
+        @callback
+        def _dummy_listener() -> None:
+            pass
+
+        self.async_add_listener(_dummy_listener)
+
+    async def _insert_statistics(self) -> None:
+        """Insert CSU Statistics."""
+        for meter in await self.api.async_get_meters():
+            id_prefix = "_".join(
+                (
+                    "CSU",
+                    meter.meter_type.name.lower(),
+                    meter.customer,
+                )
+            )
+            consumption_statistic_id = f"{DOMAIN}:{id_prefix}_energy_consumption"
+            _LOGGER.debug(
+                "Updating Statistics for %s",
+                consumption_statistic_id,
+            )
+
+            last_stat = await get_instance(self.hass).async_add_executor_job(
+                get_last_statistics, self.hass, 1, consumption_statistic_id, True, set()
+            )
+            if not last_stat:
+                _LOGGER.debug("Updating statistics for the first time")
+                usage_reads = await self._async_get_usage_reads(
+                    meter, self.api.utility.timezone()
+                )
+                consumption_sum = 0.0
+                last_stats_time = None
+            else:
+                usage_reads = await self._async_get_usage_reads(
+                    meter,
+                    self.api.utility.timezone(),
+                    last_stat[consumption_statistic_id][0]["start"],
+                )
+                if not usage_reads:
+                    _LOGGER.debug("No recent usage data. Skipping update")
+                    continue
+                stats = await get_instance(self.hass).async_add_executor_job(
+                    statistics_during_period,
+                    self.hass,
+                    usage_reads[0].start_time,
+                    None,
+                    {consumption_statistic_id},
+                    "hour" if meter.meter_type == MeterType.ELEC else "day",
+                    None,
+                    {"sum"},
+                )
+                consumption_sum = cast(float, stats[consumption_statistic_id][0]["sum"])
+                last_stats_time = stats[consumption_statistic_id][0]["start"]
+
+            consumption_statistics = []
+
+            for usage_read in usage_reads:
+                start = usage_read.start_time
+                if last_stats_time is not None and start.timestamp() <= last_stats_time:
+                    continue
+                consumption_sum += usage_read.consumption
+
+                consumption_statistics.append(
+                    StatisticData(
+                        start=start, state=usage_read.consumption, sum=consumption_sum
+                    )
+                )
+
+            name_prefix = f"CSU {meter.meter_type.name.lower()} {meter.customer}"
+
+            consumption_metadata = StatisticMetaData(
+                has_mean=False,
+                has_sum=True,
+                name=f"{name_prefix} consumption",
+                source=DOMAIN,
+                statistic_id=consumption_statistic_id,
+                unit_of_measurement=UnitOfEnergy.KILO_WATT_HOUR
+                if meter.meter_type == MeterType.ELEC
+                else UnitOfVolume.CENTUM_CUBIC_FEET,
+            )
+
+            async_add_external_statistics(
+                self.hass, consumption_metadata, consumption_statistics
+            )
+
+    async def _async_get_usage_reads(
+        self, meter: Meter, time_zone_str: str, start_time: float | None = None
+    ) -> list[UsageRead]:
+        """Get usage reads.
+
+        If start_time is None, get usage reads since account activation, otherwise since start_time - 30 days to allow corrections in data from utilities.
+
+        We read at different resolutions depending on age:
+        - day resolution for past 3 years
+        - hour resolution for past 2 months
+        - maybe quarter hour for elec?
+        """
+
+        def _update_with_finer_usage_reads(
+            usage_reads: list[UsageRead], finer_usage_reads: list[UsageRead]
+        ) -> None:
+            for i, usage_read in enumerate(usage_reads):
+                for j, finer_usage_read in enumerate(finer_usage_reads):
+                    if usage_read.start_time == finer_usage_read.start_time:
+                        usage_reads[i:] = finer_usage_reads[j:]
+                        return
+                    if usage_read.end_time == finer_usage_read.start_time:
+                        usage_reads[i + 1 :] = finer_usage_reads[j:]
+                        return
+                    if usage_read.end_time < finer_usage_read.start_time:
+                        break
+            usage_reads += finer_usage_reads
+
+        tz = await dt_util.async_get_time_zone(time_zone_str)
+        if start_time is None:
+            start = None
+        else:
+            start = datetime.fromtimestamp(start_time, tz=tz) - timedelta(days=30)
+        end = dt_util.now(tz)
+        usage_reads = await self.api.async_get_usage_reads(
+            meter, AggregateType.DAY, start, end
+        )
+        if meter.read_resolution == ReadResolution.DAY:
+            return usage_reads
+
+        if start_time is None:
+            start = end - timedelta(days=2 * 30)
+        else:
+            assert start
+            start = max(start, end - timedelta(days=2 * 30))
+        hourly_usage_reads = await self.api.async_get_usage_reads(
+            meter, AggregateType.HOUR, start, end
+        )
+        _update_with_finer_usage_reads(usage_reads, hourly_usage_reads)
+        return usage_reads
+
+        # add in additional update for quarter-hour reads?
---
+++ homeassistant/components/csu/csu.py
@@ -0,0 +1,362 @@
+import dataclasses
+from datetime import datetime
+from enum import Enum
+import json
+from typing import Any, Optional
+
+import aiohttp
+from aiohttp.client_exceptions import ClientResponseError
+import arrow
+
+from .const import TIME_ZONE, USER_AGENT
+from .exceptions import CannotConnect, InvalidAuth
+
+
+@dataclasses.dataclass
+class Customer:
+    """Data about a customer."""
+
+    customerId: str
+    customerContext: dict
+
+
+class AggregateType(Enum):
+    """How to aggregate historical data."""
+
+    BILL = "bill"
+    DAY = "day"
+    HOUR = "hour"
+    QUARTER = "quarter"
+
+    def __str__(self):
+        """Return the value of the enum."""
+        return self.value
+
+
+class MeterType(Enum):
+    """Meter type. Electric, gas, or water."""
+
+    ELEC = "ELEC"
+    GAS = "GAS"
+    WATER = "WATER"
+
+    def __str__(self):
+        """Return the value of the enum."""
+        return self.value
+
+
+@dataclasses.dataclass
+class UsageRead:
+    """A read from the meeter that has consumption data."""
+
+    start_time: datetime
+    end_time: datetime
+    consumption: float
+
+
+@dataclasses.dataclass
+class CostRead:
+    """A read from the meter that has both consumption and cost data."""
+
+    start_time: datetime
+    end_time: datetime
+    consumption: float  # taken from value field, in KWH or CCF
+    provided_cost: float  # in $
+
+
+class ReadResolution(Enum):
+    """Minimum supported resolution."""
+
+    BILLING = "BILLING"
+    DAY = "DAY"
+    HOUR = "HOUR"
+    QUARTER_HOUR = "QUARTER_HOUR"
+
+    def __str__(self):
+        """Return the value of the enum."""
+        return self.value
+
+
+SUPPORTED_AGGREGATE_TYPES = {
+    ReadResolution.BILLING: [AggregateType.BILL],
+    ReadResolution.DAY: [AggregateType.BILL, AggregateType.DAY],
+    ReadResolution.HOUR: [AggregateType.BILL, AggregateType.DAY, AggregateType.HOUR],
+    ReadResolution.QUARTER_HOUR: [
+        AggregateType.BILL,
+        AggregateType.DAY,
+        AggregateType.HOUR,
+    ],
+}
+
+
+@dataclasses.dataclass
+class Meter:
+    """Data about a Meter."""
+
+    customer: Customer
+    meterNumber: int
+    serviceNumber: str
+    serviceId: str
+    contractNum: str
+    meter_type: MeterType
+    read_resolution: Optional[ReadResolution]
+
+
+class CSU:
+    """Class that can get historical usage/cost from Colorado Springs Utilities."""
+
+    def __init__(
+        self, session: aiohttp.ClientSession, username: str, password: str
+    ) -> None:
+        self.session = session
+        self.username = username
+        self.password = password
+        self.access_token = ""
+        self.customers = [Customer]
+        self.meters = [Meter]
+
+    async def async_login(self) -> None:
+        customerId = ""
+        data = aiohttp.FormData()
+        data.add_field("username", self.username)
+        data.add_field("password", self.password)
+        data.add_field("mfa", "")
+        data.add_field("grant_type", "password")
+        try:
+            async with self.session.post(
+                "https://myaccount.csu.org/rest/oauth/token",
+                data=data,
+                headers={
+                    "User-Agent": USER_AGENT,
+                    "Authorization": "Basic d2ViQ2xpZW50SWRQYXNzd29yZDpzZWNyZXQ=",
+                },
+                raise_for_status=True,
+            ) as resp:
+                result = await resp.json()
+                if "errorMsg" in result:
+                    raise InvalidAuth(result["errorMsg"])
+                else:
+                    self.access_token = result["access_token"]
+                    customerId = result["user"]["customerId"]
+                    # self.customers.append(Customer(customerId=result['user']["customerId"]))
+                    print(self.access_token)
+
+        except ClientResponseError as err:
+            if err.status in (401, 403):
+                raise InvalidAuth(err)
+            else:
+                raise CannotConnect(err)
+
+        try:
+            async with self.session.post(
+                "https://myaccount.csu.org/rest/account/list/",
+                data=json.dumps({"customerId": customerId}),
+                headers={
+                    "User-Agent": USER_AGENT,
+                    "Authorization": "Bearer " + self.access_token,
+                    "Content-Type": "application/json",
+                },
+            ) as resp:
+                result = await resp.json()
+                if "errorMsg" in result:
+                    raise InvalidAuth(result["errorMsg"])
+                else:
+                    customerContext = result["account"][0]
+                    self.customers.append(
+                        Customer(customerId=customerId, customerContext=customerContext)
+                    )
+                    print(self.customers)
+        except ClientResponseError as err:
+            if err.status in (401, 403):
+                raise InvalidAuth(err)
+            else:
+                raise CannotConnect(err)
+
+    async def async_get_meters(self) -> None:
+        try:
+            async with self.session.post(
+                "https://myaccount.csu.org/rest/account/services/",
+                data=json.dumps(
+                    {
+                        "customerId": self.customers[0].customerId,
+                        "multiAcctLimit": 10,
+                        "accountContext": self.customers[0].customerContext,
+                    }
+                ),
+                headers={
+                    "User-Agent": USER_AGENT,
+                    "Authorization": "Bearer " + self.access_token,
+                    "Content-Type": "application/json",
+                },
+            ) as resp:
+                result = await resp.json()
+                if "errorMsg" in result:
+                    raise InvalidAuth(result["errorMsg"])
+                else:
+                    meters = result["accountSummaryType"]["servicesForGraph"]
+                    for meter in meters:
+                        if meter["serviceNumber"] == "G-TYPICAL":
+                            meterType = MeterType.GAS
+                            readFrequency = ReadResolution.DAY
+                        elif meter["serviceNumber"] == "W-TYPICAL":
+                            meterType = MeterType.WATER
+                            readFrequency = ReadResolution.DAY
+                        elif meter["serviceNumber"] == "E-TYPICAL":
+                            meterType = MeterType.ELEC
+                            readFrequency = ReadResolution.QUARTER_HOUR
+                        self.meters.append(
+                            Meter(
+                                customer=self.customers[0],
+                                meterNumber=meter["meterNumber"],
+                                serviceNumber=meter["serviceNumber"],
+                                serviceId=meter["serviceId"],
+                                contractNum=meter["serviceContract"],
+                                meter_type=meterType,
+                                read_resolution=readFrequency,
+                            )
+                        )
+                    print(self.meters)
+        except ClientResponseError as err:
+            if err.status in (401, 403):
+                raise InvalidAuth(err)
+            else:
+                raise CannotConnect(err)
+
+    async def async_get_usage_reads(
+        self,
+        meter: Meter,
+        aggregate_type: AggregateType,
+        start_date: Optional[datetime] = None,
+        end_date: Optional[datetime] = None,
+    ) -> list[UsageRead]:
+        """Get usage reads for a meter."""
+        if not meter.read_resolution:
+            raise ValueError("Meter does not have a read resolution")
+        if aggregate_type not in SUPPORTED_AGGREGATE_TYPES[meter.read_resolution]:
+            raise ValueError(f"Unsupported aggregate type for {meter.read_resolution}")
+        if not start_date:
+            raise ValueError("start_date is required")
+        if not end_date:
+            raise ValueError("end_date is required")
+        if start_date > end_date:
+            raise ValueError("start_date must be before end_date")
+
+        reads = await self.async_get_dated_data(
+            meter=meter,
+            url="https://myaccount.csu.org/rest/usage/detail/",
+            aggregate_type=aggregate_type,
+            start_date=start_date,
+            end_date=end_date,
+        )
+        if (
+            aggregate_type == AggregateType.QUARTER
+            or aggregate_type == AggregateType.HOUR
+        ):
+            meterReadField = "readDateTime"
+            consumptionField = "scaledRead"
+        else:
+            meterReadField = "readDate"
+            consumptionField = "usageConsumptionValue"
+
+        result = []
+        readStart = start_date
+        for read in reads:
+            if read[meterReadField] is not None:
+                result.append(
+                    UsageRead(
+                        # can perhaps just take readDateTime and subtract read["intervalType"]?
+                        start_time=readStart,
+                        end_time=datetime.fromisoformat(read[meterReadField]),
+                        consumption=read[consumptionField],
+                    )
+                )
+                readStart = datetime.fromisoformat(read[meterReadField])
+        return result
+
+    async def async_get_dated_data(
+        self,
+        meter: Meter,
+        url: str,
+        aggregate_type: AggregateType,
+        start_date: datetime | None = None,
+        end_date: datetime | None = None,
+    ) -> list[Any]:
+        """Wrapper for _async_fetch to break large requests into smaller batches."""
+        if start_date is None:
+            raise ValueError("start_date is required")
+        if end_date is None:
+            raise ValueError("end_date is required")
+
+        start = arrow.get(start_date.date(), TIME_ZONE)
+        end = arrow.get(end_date.date(), TIME_ZONE)
+
+        max_request_days = 30
+        if aggregate_type == AggregateType.DAY:
+            max_request_days = 6
+            url_end = "month"
+        if (
+            aggregate_type == AggregateType.QUARTER
+            or aggregate_type == AggregateType.HOUR
+        ):
+            max_request_days = 1
+            url_end = "day"
+
+        url = url + url_end
+        print(url)
+        result: list[Any] = []
+        req_end = end
+        while True:
+            req_start = start
+            if max_request_days is not None:
+                req_start = max(start, req_end.shift(days=-max_request_days))
+            if req_start >= req_end:
+                return result
+            reads = await self._async_fetch(meter, url, req_start, req_end)
+            if not reads:
+                return result
+            result = reads + result
+            # req_end = req_start.shift(days=-1)
+            req_end = req_end.shift(days=-max_request_days)
+
+    async def _async_fetch(
+        self,
+        meter: Meter,
+        url: str,
+        start_date: datetime | arrow.Arrow | None = None,
+        end_date: datetime | arrow.Arrow | None = None,
+    ) -> list[Any]:
+        data = {
+            "customerId": meter.customer.customerId,
+            "meterNumber": meter.meterNumber,
+            "serviceNumber": meter.serviceNumber,
+            "serviceId": meter.serviceId,
+            "accountContext": meter.customer.customerContext,
+            "contractNum": meter.contractNum,
+        }
+        headers = self._get_headers()
+        headers["Content-Type"] = "application/json"
+
+        if start_date:
+            data["fromDate"] = start_date.date().strftime("%Y-%m-%d %H:%M")
+        if end_date:
+            data["toDate"] = end_date.date().strftime("%Y-%m-%d %H:%M")
+        try:
+            async with self.session.post(
+                url, data=json.dumps(data), headers=headers, raise_for_status=True
+            ) as resp:
+                result = await resp.json()
+                # if DEBUG_LOG_RESPONSE:
+                #   _LOGGER.debug("Fetched: %s", json.dumps(result,indent=2))
+                return result["history"]
+        except ClientResponseError as err:
+            # Ignore server errors for BILL requests
+            # that can happen if end_date is before account activation
+            if err.status == 500:
+                return []
+            raise err
+
+    def _get_headers(self):
+        headers = {"User-Agent": USER_AGENT}
+        if self.access_token:
+            headers["authorization"] = f"Bearer {self.access_token}"
+        return headers
